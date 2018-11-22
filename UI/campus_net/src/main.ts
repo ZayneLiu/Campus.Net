6b8916740c6ac331da58c82278e58fd89dd47c8b
Zayne <llz981011@hotmail.com>
2018-11-22 18:15:33 +0800

    add vuex, vuelidate

--- UI/campus_net/src/main.ts
+++ UI/campus_net/src/main.ts
@@ -1,9 +1,14 @@
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
+import Vuex from 'vuex';
 import store from './store';
+import Vuelidate from 'vuelidate';
 
+// Vue.prototype = axios;
 Vue.config.productionTip = false;
+Vue.use(Vuex);
+Vue.use(Vuelidate);
 
 new Vue({
   router,
--- UI/campus_net/src/store.ts
+++ UI/campus_net/src/store.ts
@@ -1,10 +1,26 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 
+import axios from 'axios';
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
+  state: {
+    currentUser: '',
+  },
+  mutations: {
+    changeUser(state, user) {
+      state.currentUser = user;
+    },
+  },
+  actions: {
+    login(loginInfo) {
+      // axios({
+      //   method: 'post',
+      //   url: 'https://localhost:5001/api/user',
+      //   responseType: 'application/json',
+      //   data: loginInfo,
+      // });
+    },
+  },
 });
