e7f377e4ca5575d75f004210e81bd0b1e2c0fe27
Zayne <llz981011@hotmail.com>
2018-11-22 18:09:23 +0800

    rename MongoHelper.cs

---
+++ APIServer/DataAccess/Mongo.cs
@@ -0,0 +1,14 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace DataAccess
+{
+	public class Mongo
+	{
+		private static MongoClient Client => new MongoClient("192.168.99.100:32768");
+		private static IMongoDatabase CampusNet => Client.GetDatabase("campus_net");
+		public IMongoCollection<BsonDocument> Posts => CampusNet.GetCollection<BsonDocument>("posts");
+		public IMongoCollection<BsonDocument> Questions => CampusNet.GetCollection<BsonDocument>("questions");
+		public IMongoCollection<BsonDocument> Users => CampusNet.GetCollection<BsonDocument>("users");
+	}
+}
