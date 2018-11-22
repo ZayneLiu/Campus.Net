61c8da3c6fc1e991f67bb3bfd5ba0c9d384fb545
Zayne <llz981011@hotmail.com>
2018-11-22 18:11:50 +0800

    测试数据通路

---
+++ APIServer/APIServer/Controllers/QuestionController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using DataAccess.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+
+namespace APIServer.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class QuestionController : ControllerBase
+	{
+		// GET api/questions
+		[HttpGet]
+		public ActionResult<object> Method()
+		{
+			return new List<Question>
+			{
+				new Question
+				{
+					Id = "id_01",
+					Votes = 21,
+					Views = 12,
+					Answers = 34,
+					Title = "Title",
+					Tags = new List<Tag>
+					{
+						new Tag
+						{
+							Id = "tag_id",
+							Name = "tag_name",
+							Description = "tag_desc"
+						},
+						new Tag
+						{
+							Id = "tag_id",
+							Name = "tag_name",
+							Description = "tag_desc"
+						}
+					}
+				}
+			};
+		}
+
+		// api/question
+		[HttpGet("{id}")]
+		public ActionResult<JObject> GetQuestionById(int id)
+		{
+			return JObject.Parse("{'name': 's', 'time': '" + DateTime.Now + "', 'id': '" + id + "'}");
+		}
+	}
+}
--- APIServer/APIServer/Controllers/UserController.cs
+++ APIServer/APIServer/Controllers/UserController.cs
@@ -1,47 +1,79 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using DataAccess.Models;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+using System.Net.Mail;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+//using MongoDB.Bson.IO;
 
 namespace APIServer.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class UserController : ControllerBase
-    {
-        // GET: api/user/
-        [HttpGet]
-        public ActionResult<string> Get()
-        {
-            return "All Users";
-        }
+	[Route("api/[controller]")]
+	[ApiController]
+	public class UserController : ControllerBase
+	{
+		// GET: api/user/
+		[HttpGet]
+		public ActionResult<JObject> Get()
+		{
+			Console.WriteLine();
+			return JObject.Parse("{}");
+		}
 
-        // GET api/user/5
-        [HttpGet("{id}")]
-        public ActionResult<string> Get(int id)
-        {
-            return string.Format("id 为 {0} 的用户", id);
-        }
+		// GET api/user/5
+		[HttpGet("{id}")]
+		public ActionResult<string> Get(int id)
+		{
+			return string.Format("id 为 {0} 的用户", id);
+		}
 
-        // POST api/user
-        [HttpPost]
-        public void Post([FromBody]string value)
-        {
-        }
+		// POST api/user
+		[HttpPost]
+		public ActionResult<string> Post([FromBody] JObject form)
+		{
+			// var login = form["login"].Value<string>();
+			// var password = form["password"].Value<string>();
+			// +++++前端已做数据校验，并重构登陆对象+++++
+			// var type = "email";
 
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
-        {
-        }
+			// try
+			// {
+			// 	// 判断是否为 Email
+			// 	new MailAddress(login);
+			// }
+			// catch
+			// {
+			// 	// 判断是否为 纯数字
+			// 	foreach (var item in login.ToCharArray())
+			// 	{
+			// 		var temp = 0;
+			// 		if (!int.TryParse(item.ToString(), out temp))
+			// 		{
+			// 			return "FAILED";
+			// 		}
+			// 	}
+			// 	type = "scid";
+			// }
 
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
-    }
+			// 构造完整登陆 用户对象
+			var loginresult = form.ToObject<User>();
+
+			// 返回登陆结果
+			return "SUCCESS";
+		}
+
+		// PUT api/values/5
+		[HttpPut("{id}")]
+		public void Put(int id, [FromBody] string value)
+		{
+		}
+
+		// DELETE api/values/5
+		[HttpDelete("{id}")]
+		public void Delete(int id)
+		{
+		}
+	}
 }
