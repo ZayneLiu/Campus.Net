f622f1ed7e00949a2f011f508c6694997eca0896
Zayne <llz981011@hotmail.com>
2018-12-09 19:12:44 +0800

    rename

---
+++ APIServer/APIServer/Controllers/QuestionsController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using DataAccess.Models;
+using DataAccess.Services;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Bson;
+using Newtonsoft.Json.Linq;
+
+namespace APIServer.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class QuestionsController : ControllerBase
+	{
+		private readonly QuestionService _questionService;
+		public QuestionsController(QuestionService questionService) => _questionService = questionService;
+
+		// GET api/questions
+		[HttpGet]
+		[Produces("application/json")]
+		public ActionResult<List<Question>> Method()
+		{
+			return new List<Question>();
+		}
+
+//		// GET api/questions/{id}
+//		[HttpGet("{id}")]
+//		[Produces("application/json")]
+//		public ActionResult<Question> GetQuestionById(string id)
+//		{
+//			return new Question
+//			{
+//				Id = id
+//			};
+//		}
+		
+		[HttpPost]
+		[Produces("application/json")]
+		public ActionResult<Question> CreateQuestion()
+		{
+			// create question
+			return  new Question();
+		}
+		
+		[HttpPut("{id}")]
+		[Produces("application/json")]
+		public ActionResult<Question> UpdateQuestion(string id, [FromBody]JObject question)
+		{
+			var questionIn = question.ToObject<Question>();
+			var questionOut = _questionService.Update(new ObjectId(id), questionIn);
+			return questionOut;
+		}
+		
+		
+	}
+}
