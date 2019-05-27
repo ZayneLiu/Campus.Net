d4b79be57e2fbeb8d31b99b826f44276ad544ef9
Zayne.Liu <llz981011@hotmail.com>
2019-05-27 15:53:01 +0800

    Create vue.d.ts
    声明 事件总线 的类型

---
+++ App/client/src/vue.d.ts
@@ -0,0 +1,12 @@
+import Vue from 'vue';
+
+declare module 'vue/types/vue' {
+  // 可以使用 `VueConstructor` 接口
+  // 来声明全局属性
+  interface Vue {
+    /**
+     * 事件总线
+     */
+    $EventBus: Vue;
+  }
+}
